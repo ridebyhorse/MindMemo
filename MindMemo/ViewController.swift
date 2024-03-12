882079aa11a08a01e01b77fa8d6f2da4170245ae
Maria Nesterova <ridebyhorse@gmail.com>
2024-03-12 18:09:36 +0300

    Initial Commit

---
+++ MindMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MindMemo
+//
+//  Created by Мария Нестерова on 12.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
